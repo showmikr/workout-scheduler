b692731bdaffeff94c11abc764b4b6931e2a8413
ruski7 <ninjasaws@gmail.com>
2023-09-01 10:12:50 -0700

    mobile auth through github

--- App.tsx
+++ App.tsx
@@ -1,11 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button, StyleSheet, View, Text } from 'react-native';
+
+const CLIENT_ID = "8a308561f3aeb41d9969";
+const CLIENT_ID_TEST_MOBILE = "c836d33329427d2050f6";
+const CLIENT_ID_TEST_WEB = "ca1743b472e63f27cefe";
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/' + CLIENT_ID_TEST_MOBILE,
+};
 
 export default function App() {
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: CLIENT_ID_TEST_MOBILE,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        preferLocalhost: true,
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    //console.log(response?.type)
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
+      <Button 
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
