8467602195319d4d38129d381cc3312122519182
Showmik Roy <showmik.roy@gmail.com>
2023-09-05 16:09:23 -0700

    Migrated to expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Page() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up index.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
