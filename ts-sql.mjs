c539ba85ec1767262c0ff20ebd1c27c18d71a49d
unknown <showmik.roy@gmail.com>
2024-01-15 00:16:10 -0800

    Can auto-generate sql types w/ only sql script
    In the previous commit, I had to use a pre-loaded sqlite database,
    ddl.sqlite, in order to auto-generate db types. Now, I can auto-generate
    these types by simply creating an in-memory database and deleting it's
    (I think) backup file. This works and it's an improvement over having to
    have an actual sqlite db in the project source code. One problem is that
    I would have preffered not needing to delete the backup 'file' - I
    couldn't figure out how to prevent it from being automatically made, but
    I might be able to avoid that if I try out the better-sqlite library
    instead of the current node-sqlite lib. We'll have to see about that
    in another commit.

--- ts-sql.mjs
+++ ts-sql.mjs
@@ -4,42 +4,84 @@ to generate typescript definitions for your sqlite database
 */
 
 import sqlts from "@rmp135/sql-ts";
-import { writeFileSync } from "node:fs";
+import { writeFileSync, readFileSync, rmSync } from "node:fs";
+import sqlite3 from "sqlite3";
+
+const sqliteThree = sqlite3.verbose(); // Allow for longer stack traces on sql errors from sqlite
+const dbFilePath = "file:temp.db"; // Not sure why but a file named 'file' is produced after closing connection
+
+const db = new sqliteThree.Database(dbFilePath);
+const sqlScript = readFileSync("./workout-scheduler-v2.sql").toString();
+
+// Use regular expression to grab all CREATE TABLE queries from sql script
+const queryStrings = sqlScript.match(/CREATE.+?\);/gs);
 
 const config = {
   client: "sqlite3",
   connection: {
-    filename: "./ddl.sqlite",
+    filename: dbFilePath,
   },
   filename: "sqlite-types",
   useNullAsDefault: true,
 };
 
-const tsString = await sqlts.toTypeScript(config);
+/* 
+Creates a new typescript definitions object with
+PascalCase table names and camelCase column names for each table
+*/
+function renameTables(definitions) {
+  const toPascalCase = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+  const toCamelCase = (word, index) =>
+    index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word;
 
-let definitions = await sqlts.toObject(config);
-const column_obj = definitions.tables[0].columns[0];
-
-definitions.tables = definitions.tables.map((table) => {
-  const newColumns = table.columns.map((column) => ({
-    ...column,
-    propertyName: column.name
-      .split("_")
-      .map((word, index) =>
-        index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word
-      )
-      .reduce((acc, word) => acc + word, ""),
-  }));
   return {
-    ...table,
-    interfaceName: table.name
-      .split("_")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .reduce((acc, word) => acc + word, ""),
-    columns: newColumns,
+    ...definitions,
+    tables: definitions.tables.map((table) => ({
+      ...table,
+      interfaceName: table.name.split("_").map(toPascalCase).join(""),
+      columns: table.columns.map((column) => ({
+        ...column,
+        propertyName: column.name.split("_").map(toCamelCase).join(""),
+      })),
+    })),
   };
-});
+}
 
-const tsTypes = sqlts.fromObject(definitions);
-writeFileSync("./sqlite-types.ts", tsTypes);
-console.log("Database types created: sqlite-types.ts");
+function main() {
+  db.serialize(() => {
+    for (const query of queryStrings) {
+      db.run(query);
+    }
+    db.all("SELECT day FROM days_of_week;", (err, rows) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(rows);
+      sqlts
+        .toObject(config)
+        .then((definitions) => {
+          const tsTypes = sqlts.fromObject(renameTables(definitions));
+          writeFileSync("./sqlite-types.ts", tsTypes);
+          console.log("Database types created: sqlite-types.ts");
+        })
+        .then(() => {
+          db.close((err) => {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("Closed db");
+          });
+        })
+        .then(() => {
+          rmSync("./file"); // Removes the temporary 'file' that gets made after closing the db
+          console.log("Deleted temporary db file");
+        })
+        .catch((reason) => {
+          console.log(reason);
+          console.log("Couldn't get definitions");
+        });
+    });
+  });
+}
+
+main();
