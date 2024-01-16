75ef4e1804164936ebde66ff05ee56f3d606bf98
unknown <showmik.roy@gmail.com>
2024-01-16 14:10:39 -0800

    removed redundant db utility defs from tab two
    While working on making a database connection to the sqlite db, I
    defined some utility functions in the app's tab two page, two.tsx for
    opening a connection to the sqlite db. I copied these utility funcitons
    into a separate module, but just forgot to delete them from tab two.
    This commit deletes those functions from two.tsx and simply imports them
    from the utility module db-utils.ts instead.

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,40 +4,7 @@ import { Text, View } from "../../components/Themed";
 import * as FileSystem from "expo-file-system";
 import * as SQLite from "expo-sqlite";
 import sqlite_ddl from "../../pgdb-ddl";
-
-const dbName = "pg-sqlite.db";
-
-async function doesLocalDbExist() {
-  const dbExists = (
-    await FileSystem.getInfoAsync(
-      FileSystem.documentDirectory + "SQLite/" + dbName
-    )
-  ).exists;
-  return dbExists;
-}
-
-// function for opening sqlite version of our postgresDB
-async function openDB() {
-  if (await doesLocalDbExist()) {
-    return SQLite.openDatabase(dbName);
-  }
-
-  // otherwise, we need to initialize the db before returning it
-  const db = SQLite.openDatabase(
-    dbName,
-    undefined,
-    undefined,
-    undefined,
-    () => {
-      for (const queryString of sqlite_ddl) {
-        db.transaction((transaction) => {
-          transaction.executeSql(queryString);
-        });
-      }
-    }
-  );
-  return db;
-}
+import { openDB, doesLocalDbExist } from "../../db-utils";
 
 export default function TabTwoScreen() {
   const readDB = () => {
