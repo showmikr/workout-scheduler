312a7bc620a07cf3f5170a511047fa51a592db7b
unknown <showmik.roy@gmail.com>
2024-01-17 17:51:42 -0800

    Early proof of concept for making db 'globally' accessible
    This is just super early work to prove to myself that you can pass a
    database connection down to as a prop to child components and synchronize with it using React's useEffect function. Now I have a clearer
    understanding of how I might take this proof of concept work and apply it to a
    proper useContext function to make the sqlite database truly globally
    accessible. React's useContext function basically does prop-drilling -
    as in, it drills a given prop all the way down to every child in the
    component tree. That's exactly what I need to make the db available to
    every page. Again, this is early days. We'll see what we can accomplish
    in a future commit.

--- app/(app)/hello/index.tsx
+++ app/(app)/hello/index.tsx
@@ -1,9 +1,74 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import * as SQLite from "expo-sqlite";
+import { openDB, doesLocalDbExist } from "../../../db-utils";
 
-export default function HelloText() {
+const deleteDB = async () => {
+  doesLocalDbExist().then((dbExists) => {
+    if (!dbExists) {
+      console.log("db doesn't exist, quitting deletion");
+      return;
+    }
+    openDB().then((db) => {
+      db.closeAsync();
+      db.deleteAsync();
+      console.log("DB deleted successfully");
+    });
+  });
+};
+
+function HelloChild({
+  database,
+  onDeleteDb,
+}: {
+  database: SQLite.Database | null;
+  onDeleteDb: () => void;
+}) {
+  const textStyle = "dark:text-white text-4xl";
+  if (!database) {
+    return <Text className={textStyle}>...Loading</Text>;
+  } else {
+    return (
+      <>
+        <Text className={textStyle}>Hello There</Text>
+        <Pressable
+          className="m-10 p-1 bg-slate-600 border-solid border-2 border-slate-400 active:opacity-50"
+          onPress={onDeleteDb}
+        >
+          <Text className="text-lg/10 dark:color-white">Delete Database</Text>
+        </Pressable>
+      </>
+    );
+  }
+}
+
+// Proof of concept for using database as "global" context
+export default function HelloParent() {
+  const [db, setDb] = React.useState<SQLite.Database | null>(null);
+  React.useEffect(() => {
+    // Using a setTimeout function to simulate loading in the db
+    // for 2 seconds b/c it normally loads too fast to even see the loading screen I wrote
+    const timeoutId = setTimeout(() => {
+      openDB().then((db) => {
+        setDb(db);
+      });
+    }, 2000);
+    // Turns out I have to return a cleanup function for the useEffect
+    // or else the seTimeOut function doesn't reset it's timer when you delete the db.
+    // It just continues to run from whatever the timer's countdown was before the deletion,
+    // meaning the "...loading" screen would generally last shorter than the assigned 2 seconds
+    // every time you pressed "Delete Database". It's an important thing to keep note of.
+    // Even timers are resources that have to be freed! (That's what the clearTimeout function does... I think)
+    return () => clearTimeout(timeoutId);
+  });
+  const onDeleteDb = () => {
+    deleteDB().then(() => {
+      setDb(null);
+    });
+  };
   return (
     <View className="dark:bg-black flex-1 items-center justify-center">
-      <Text className="dark:text-white text-4xl">Hello There</Text>
+      <HelloChild database={db} onDeleteDb={onDeleteDb} />
     </View>
   );
 }
