0b1d33f2ad6bcdc014fbbfd2092bfd7861cfe13f
unknown <showmik.roy@gmail.com>
2024-01-19 03:24:18 -0800

    Added back button to home page modal ui

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
+import { router } from "expo-router";
+import { Button } from "react-native";
 
 export default function () {
   return (
     <Stack screenOptions={{ animation: "default", headerShown: true }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+      <Stack.Screen
+        name="modal"
+        options={{
+          presentation: "modal",
+          headerLeft: () => (
+            <Button onPress={() => router.back()} title="Back" />
+          ),
+          headerBackVisible: true,
+        }}
+      />
     </Stack>
   );
 }
