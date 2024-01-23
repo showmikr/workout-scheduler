aafd3ab24c864e2352174ad8744abf534d9a833c
unknown <showmik.roy@gmail.com>
2024-01-23 12:56:29 -0800

    upgraded expo to sdk 50
    Had some warnings when upgrading, react-native-reanimated in particular
    doesn't play nice with nativewind. I get a warning with reanimated
    saying that the native version cannot be determined and the root cause
    is the nativewind lib. Also, nativewind seems to be making it impossible
    to use the app in the web browser. Nativewind is
    nice, but it's causing a lot of pain to co-exist right now. I'll
    consider disabling nativewind on web by editing the metro config later and
    perhaps remove nativewind altogether, but that's for another commit.

--- babel.config.js
+++ babel.config.js
@@ -5,10 +5,5 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-      'react-native-reanimated/plugin'
-    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -3,10 +3,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push("db");
 
