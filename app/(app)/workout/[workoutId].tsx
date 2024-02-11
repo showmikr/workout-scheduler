5b2a68565c6133a7fe6261cb30ddb0357a616e51
Showmik Roy <showmik.roy@gmail.com>
2024-02-11 08:29:51 -0800

    early days SectionList implemented for workout page
    I've also attempted to make the queries and data type safe, but I've run
    into some issues w/ regards to react's SectionList sections prop which
    doesn't seem to play nice with data of type 'UnifiedResistanceSet[] |
    UnifiedCardioSet[]' It loooks like it only plays nice when the data
    property of my section array is of type '(UnifiedResistanceSet |
    UnifiedCardioSet)[]' This means react expects a unified array type, not
    a split one. I'd like the split version as that's more the correct type
    definition of my data, but we'll have to see about that in a later
    commit.

--- app/(app)/workout/[workoutId].tsx
+++ app/(app)/workout/[workoutId].tsx
@@ -1,18 +1,25 @@
 import { useLocalSearchParams } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite/next";
-import { Fragment, ReactElement } from "react";
-import { Text, SafeAreaView, View, ScrollView } from "react-native";
+import { Text, SafeAreaView, View, SectionList } from "react-native";
 
 // hard coded constants based on the sqlite db table "exercise_type"
 const RESISTANCE_ENUM = 1;
 const CARDIO_ENUM = 2;
-type ExerciseType = typeof RESISTANCE_ENUM | typeof CARDIO_ENUM;
 
-type ExerciseParams = {
-  id: number;
+type CardioExercise = {
+  exercise_id: number;
+  exercise_type_id: typeof CARDIO_ENUM;
   title: string;
-  exercise_type_id: ExerciseType;
 };
+
+type ResistanceExercise = {
+  exercise_id: number;
+  exercise_type_id: typeof RESISTANCE_ENUM;
+  title: string;
+};
+
+type ExerciseParams = CardioExercise | ResistanceExercise;
+
 type ExerciseSetParams = {
   exercise_set_id: number;
   list_order: number;
@@ -33,116 +40,21 @@ type CardioSetParams = {
 type UnifiedResistanceSet = ExerciseSetParams & ResistanceSetParams;
 type UnifiedCardioSet = ExerciseSetParams & CardioSetParams;
 
-type ExerciseSetsObject =
-  | {
-      exerciseType: typeof CARDIO_ENUM;
-      exerciseSets: UnifiedCardioSet[];
-    }
-  | {
-      exerciseType: typeof RESISTANCE_ENUM;
-      exerciseSets: UnifiedResistanceSet[];
-    };
-
-// Currently not in use, just playing around w/ how to structure state
-type ExerciseAggregate =
-  | {
-      exerciseType: typeof CARDIO_ENUM;
-      exercise: ExerciseParams;
-      exerciseSets: UnifiedCardioSet[];
-    }
-  | {
-      exerciseType: typeof RESISTANCE_ENUM;
-      exercise: ExerciseParams;
-      exerciseSets: UnifiedResistanceSet[];
-    };
-
-const CommonInfo = ({
-  exerciseSet,
-  children,
-}: {
-  exerciseSet: ExerciseSetParams;
-  children?: ReactElement<Text>; // Trying to make this text. Typescript doesn't seem to enforce text children.
-}) => (
-  <Text className="text-xl dark:text-white">
-    {exerciseSet.title}
-    {"    "}
-    Reps: {exerciseSet.reps}
-    {"    "}
-    Rest: {exerciseSet.rest_time}s{"    "}
-    {children}
-  </Text>
-);
-
-function ExerciseSetsList({
-  exerciseObject,
-}: {
-  exerciseObject: ExerciseSetsObject;
-}) {
-  const { exerciseType, exerciseSets } = exerciseObject;
-  return (
-    <View className="pb-2">
-      {exerciseType === RESISTANCE_ENUM ?
-        exerciseSets.map((set) => (
-          <Fragment key={set.exercise_set_id}>
-            <CommonInfo exerciseSet={set}>
-              <Text className="text-xl dark:text-white">
-                {set.total_weight}kg
-              </Text>
-            </CommonInfo>
-          </Fragment>
-        ))
-      : exerciseSets.map((set) => (
-          <Fragment key={set.exercise_set_id}>
-            <CommonInfo exerciseSet={set}>
-              <Text className="text-xl dark:text-white">
-                Target Distance:{" "}
-                {set.target_distance ? set.target_distance + "m" : "null"}
-                {"    "}
-                Target Speed:{" "}
-                {set.target_speed ? set.target_speed + "m/s" : "null"}
-                {"    "}
-                Target Time: {set.target_time ? set.target_time + "s" : "null"}
-              </Text>
-            </CommonInfo>
-          </Fragment>
-        ))
-      }
-    </View>
-  );
-}
-
 export default function WorkoutDetails() {
   const { workoutId } = useLocalSearchParams<{ workoutId: string }>();
   const db = useSQLiteContext();
 
   const exercises = db.getAllSync<ExerciseParams>(
-    `SELECT ex.id, ex.title, ex.exercise_type_id FROM exercise as ex
+    `SELECT ex.id AS exercise_id, ex.title, ex.exercise_type_id FROM exercise as ex
       WHERE ex.workout_id = ?`,
     workoutId
   );
 
-  const getExerciseSets = (exerciseId: number, exerciseType: ExerciseType) => {
-    if (exerciseType === CARDIO_ENUM) {
-      const sets = db.getAllSync<UnifiedCardioSet>(
-        `SELECT
-          exercise_set.id AS exercise_set_id,
-          exercise_set.list_order,
-          exercise_set.reps,
-          exercise_set.rest_time,
-          exercise_set.title,
-          cardio_set.id AS cardio_set_id,
-          cardio_set.target_distance,
-          cardio_set.target_speed,
-          cardio_set.target_time
-        FROM exercise_set 
-          INNER JOIN cardio_set ON exercise_set.id = cardio_set.exercise_set_id 
-          WHERE exercise_set.exercise_id = ?`,
-        exerciseId
-      );
-      return { exerciseType: CARDIO_ENUM, exerciseSets: sets } as const;
-    } else {
-      // Otherwise, assume it's a resistance set
-      const sets = db.getAllSync<UnifiedResistanceSet>(
+  const sectionData = exercises.map((ex) => {
+    if (ex.exercise_type_id === RESISTANCE_ENUM) {
+      // This is a hack to satisfy react's Section data prop,
+      // normally it would be more appropriate to use UnifiedResistanceSet only
+      const sets = db.getAllSync<UnifiedResistanceSet | UnifiedCardioSet>(
         `SELECT 
           exercise_set.id AS exercise_set_id,
           exercise_set.list_order,
@@ -154,34 +66,86 @@ export default function WorkoutDetails() {
         FROM exercise_set 
         INNER JOIN resistance_set ON exercise_set.id = resistance_set.exercise_set_id 
         WHERE exercise_set.exercise_id = ?`,
-        exerciseId
+        ex.exercise_id
       );
-      return { exerciseType: RESISTANCE_ENUM, exerciseSets: sets } as const;
+      return {
+        exercise: ex,
+        exerciseType: ex.exercise_type_id,
+        data: sets,
+        key: ex.exercise_id.toString(),
+      };
     }
-  };
+    // This is a hack to satisfy react's Section data prop,
+    // normally it would be more appropriate to use UnifiedCardioSet only
+    const sets = db.getAllSync<UnifiedCardioSet | UnifiedResistanceSet>(
+      `SELECT
+        exercise_set.id AS exercise_set_id,
+        exercise_set.list_order,
+        exercise_set.reps,
+        exercise_set.rest_time,
+        exercise_set.title,
+        cardio_set.id AS cardio_set_id,
+        cardio_set.target_distance,
+        cardio_set.target_speed,
+        cardio_set.target_time
+      FROM exercise_set 
+      INNER JOIN cardio_set ON exercise_set.id = cardio_set.exercise_set_id 
+      WHERE exercise_set.exercise_id = ?`,
+      ex.exercise_id
+    );
+    return {
+      exercise: ex,
+      exerciseType: ex.exercise_type_id,
+      data: sets,
+      key: ex.exercise_id.toString(),
+    };
+  });
 
   return (
     <SafeAreaView className="flex-1 justify-center">
-      <ScrollView>
-        <View className="items-center pb-8 pt-8">
-          <Text className="text-3xl dark:text-white">
-            Workout Id: {workoutId}
-          </Text>
-          <Text className="text-3xl dark:text-white">Exercise List</Text>
-        </View>
-        <View className="flex-1 pl-4">
-          {exercises.map((ex) => {
-            return (
-              <View key={ex.id} className="pb-4">
-                <Text className="text-2xl dark:text-white">{ex.title}</Text>
-                <ExerciseSetsList
-                  exerciseObject={getExerciseSets(ex.id, ex.exercise_type_id)}
-                />
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
+      <View className="items-center pb-8 pt-8">
+        <Text className="text-3xl dark:text-white">
+          Workout Id: {workoutId}
+        </Text>
+        <Text className="text-3xl dark:text-white">Exercise List</Text>
+      </View>
+      <SectionList
+        sections={sectionData}
+        keyExtractor={(item, _index) => {
+          return item.exercise_set_id.toString();
+        }}
+        renderSectionHeader={({ section: { exercise } }) => (
+          <View className="bg-black pl-4">
+            <Text className="text-3xl font-bold dark:text-white">
+              {exercise.title}
+            </Text>
+          </View>
+        )}
+        renderItem={({ item, index }) => {
+          return (
+            <Text className="pl-4 text-xl dark:text-white">
+              {item.title}
+              {"    "}
+              Reps: {item.reps}
+              {"    "}
+              Rest: {item.rest_time}s{"    "}
+              {"resistance_set_id" in item ?
+                <Text>{item.total_weight}kg</Text>
+              : <Text className="text-xl dark:text-white">
+                  Target Distance:{" "}
+                  {item.target_distance ? item.target_distance + "m" : "null"}
+                  {"    "}
+                  Target Speed:{" "}
+                  {item.target_speed ? item.target_speed + "m/s" : "null"}
+                  {"    "}
+                  Target Time:{" "}
+                  {item.target_time ? item.target_time + "s" : "null"}
+                </Text>
+              }
+            </Text>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 }
