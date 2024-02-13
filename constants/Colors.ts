4001d446e1f919279d06a423a872c517758fa28d
Showmik Roy <showmik.roy@gmail.com>
2024-02-13 15:37:35 -0800

    Added neutral grey color constants from tailwind
    This is for when I'll later need the grey colors for styling the color
    attributes of icons and images in the early UI prototypes.

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,20 @@
 const tintColorLight = "#2f95dc";
 const tintColorDark = "#fff";
 
+const twNeutralColors = {
+  neutral50: "rgb(250 250 250)",
+  neutral100: "rgb(245 245 245)",
+  neutral200: "rgb(229 229 229)",
+  neutral300: "rgb(212 212 212)",
+  neutral400: "rgb(163 163 163)",
+  neutral500: "rgb(115 115 115)",
+  neutral600: "rgb(82 82 82)",
+  neutral700: "rgb(64 64 64)",
+  neutral800: "rgb(38 38 38)",
+  neutral900: "rgb(23 23 23)",
+  neutral950: "rgb(10 10 10)",
+};
+
 export default {
   light: {
     text: "#000",
@@ -17,3 +31,5 @@ export default {
     tabIconSelected: tintColorDark,
   },
 };
+
+export { twNeutralColors };
