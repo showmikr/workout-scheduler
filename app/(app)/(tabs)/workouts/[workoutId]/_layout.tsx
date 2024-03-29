b33ed811bf674e70c00b3a42f1f3af90199369c1
Showmik Roy <showmik.roy@gmail.com>
2024-03-29 10:41:04 -0700

    tiny formatting refactor to workout layout page code

--- app/(app)/(tabs)/workouts/[workoutId]/_layout.tsx
+++ app/(app)/(tabs)/workouts/[workoutId]/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function () {
+export default function() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
