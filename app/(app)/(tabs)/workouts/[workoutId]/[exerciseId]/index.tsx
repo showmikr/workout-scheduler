4e9d2669bf775b1ab102f1fcff2a930b5f5d4401
Showmik Roy <showmik.roy@gmail.com>
2024-04-11 12:27:39 -0700

    ui: made resistance set card for exercise sets page (no state logic yet)

--- app/(app)/(tabs)/workouts/[workoutId]/[exerciseId]/index.tsx
+++ app/(app)/(tabs)/workouts/[workoutId]/[exerciseId]/index.tsx
@@ -1,4 +1,11 @@
-import { SafeAreaView, Text } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite/next";
 import { useState } from "react";
@@ -9,6 +16,7 @@ import {
   UnifiedResistanceSet,
   exerciseEnums,
 } from "../../../../../../utils/exercise-types";
+import { twColors } from "../../../../../../constants/Colors";
 
 function useExerciseData() {
   const { exerciseId } = useLocalSearchParams<{ exerciseId: string }>();
@@ -105,28 +113,101 @@ export default function () {
       </SafeAreaView>
     );
   }
+
   return (
-    <SafeAreaView>
-      <Text className="text-3xl text-black dark:text-white">
-        Hello, I'm an exercise page placeholder
-      </Text>
-      <Text className="text-3xl text-black dark:text-white">
-        Exercise ID: {exerciseId}
-      </Text>
-      {exerciseSection.data.map((item) => {
-        return (
-          <Text
-            key={item.exercise_set_id}
-            className="text-xl text-black dark:text-white"
-          >
-            {item.title}
-            {"    "}
-            Reps: {item.reps}
-            {"    "}
-            Rest:{item.rest_time}s
-          </Text>
-        );
-      })}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={{ borderWidth: 1, borderColor: "red" }}>
+        <Text className="text-3xl text-black dark:text-white">
+          Hello, I'm an exercise page placeholder
+        </Text>
+        <Text className="text-3xl text-black dark:text-white">
+          Exercise ID: {exerciseId}
+        </Text>
+        {exerciseSection.data.map((item) => {
+          return (
+            <ResistanceSet
+              set={item as UnifiedResistanceSet}
+              key={item.exercise_set_id}
+            />
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
+const ResistanceSet = ({ set }: { set: UnifiedResistanceSet }) => {
+  return (
+    <View
+      style={[
+        setStyles.rowContainer,
+        setStyles.inline,
+        { justifyContent: "space-around" },
+      ]}
+    >
+      <View>
+        <Text style={setStyles.inputLabel}>Reps</Text>
+        <TextInput
+          inputMode="decimal"
+          value={set.reps.toString()}
+          maxLength={5}
+          style={[setStyles.textInput, setStyles.inertInputState]}
+        />
+      </View>
+      <View>
+        <Text style={setStyles.inputLabel}>Weight</Text>
+        <View style={setStyles.inline}>
+          <TextInput
+            inputMode="decimal"
+            value={set.total_weight.toFixed(2)}
+            style={[setStyles.textInput, setStyles.inertInputState]}
+          />
+          <Text style={setStyles.inputLabel}>kg</Text>
+        </View>
+      </View>
+      <View>
+        <Text style={setStyles.inputLabel}>Rest</Text>
+        <View style={setStyles.inline}>
+          <TextInput
+            inputMode="numeric"
+            value={set.rest_time.toString()}
+            style={[setStyles.textInput, setStyles.inertInputState]}
+          />
+          <Text style={setStyles.inputLabel}>s</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const setStyles = StyleSheet.create({
+  inputLabel: {
+    fontSize: 18,
+    color: "white",
+    textAlign: "left",
+    paddingBottom: 2,
+  },
+  textInput: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "right",
+  },
+  inline: {
+    flexDirection: "row",
+  },
+  rowContainer: {
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  inertInputState: {
+    backgroundColor: twColors.neutral800,
+    minWidth: 32,
+    borderRadius: 5,
+    paddingLeft: 2,
+    paddingRight: 2,
+  },
+  activeInputState: {
+    backgroundColor: twColors.neutral100,
+    color: "black",
+  },
+});
