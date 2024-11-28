66630925034f68370243f36836a9bc087e20bcde
Showmik Roy <showmik.roy@gmail.com>
2024-11-28 04:48:18 -0800

    removed nativewind dependency and did setup for iOS dev build
    I removed all code related to nativewind and uninstalled any dependencies
    related. However, removing this dependency also caused me some headaches trying
    to get the app to run an iOS development build. When I uninstalled the nativewind
    dependency, it seemed to cause a crash on the iOS development build saying there was
    and unresolved dependency for a "react-native-css-interop" package in the expo-router
    node_modules files. I was flabbergasted and confused since "react-native-css-interop"
    was literally apart of the nativewind source code (WHICH I UNINSTALLED ALREADY!), so
    you can understand my surprise when I see that for some reason, the iOS development
    build was saying that this dependency was needed despite the fact that it was NOT used at all!
    Anyways, I think I figured out the problem: you see, I did multiple clean from-scratch runs
    using `expo run:ios --build-no-cache`, but the one thing I didn't think to do was run
    `expo start -c` after I removed all the nativewind-dependent code. So, I ran this command and
    what do you know, `expo run:ios` works fine. So, main lesson is the `expo run:ios` command
    doesn't incorporate updating your Javascript/Typescript files, so if you make some big changes
    in say, your metro.config.js or your other JS/TS files, then you better run `expo start -c` beforehand
    b/c `expo run:ios` just builds the iOS related files, not the JS stuff too.

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 // @type {import('expo/metro-config').MetroConfig}
 /**
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,6 @@ import { Slot, SplashScreen } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { SessionProvider } from "@/context/session-provider";
-import "../../global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
